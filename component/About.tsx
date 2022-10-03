5935e686d692c4e55b367227480a3b74779b730b
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-10-03 23:33:48 +0100

    second section

--- component/About.tsx
+++ component/About.tsx
@@ -4,29 +4,60 @@ import styles from "./../styles/Home.module.scss";
 const About = () => {
   return (
     <section className={styles.about}>
-      <p>About Me</p>
+      <p>ABOUT ME</p>
       <h1>A bit about me.</h1>
-      <p>
+      <p className={styles.summary}>
         I am Miskiat,a Frontend Software Engineer experienced in designing,
         developing, and managing apps and internal frameworks. I am a deep
         creative thinker with a keen eye for details.I specialize in ReactJS,
         Typescript, Next.js and responsive design.
       </p>
       <div className={styles.skills}>
-        <div>
+        <div className={styles.language}>
           Languages
-          <p>HTML</p>
-          <p>CSS</p>
-          <p>JAVASCRIPT</p>
-          <p>TYPESCRIPT</p>
+          <div className={styles.languages}>
+            <p>
+              {" "}
+              <img src="right.svg" /> HTML
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> CSS
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> JAVASCRIPT
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> TYPESCRIPT
+            </p>
+          </div>
         </div>
-        <div>
+        <div className={styles.technology}>
           Technologies
-          <p>REACT NATIVE</p>
-          <p>REACT</p>
-          <p>TAILWIND</p>
-          <p>GIT</p>
-          <p>NETLIFY</p>
+          <div className={styles.technologys}>
+            <p>
+              {" "}
+              <img src="right.svg" /> REACT NATIVE
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> REACT
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> TAILWIND
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> GIT
+            </p>
+            <p>
+              {" "}
+              <img src="right.svg" /> NETLIFY
+            </p>
+          </div>
         </div>
       </div>
     </section>
