5a074c1ed5608533071785e5bec2fe4f0f748269
Miskiat Badmus <badmusmiskiat@gmail.com>
2022-10-09 02:44:25 +0100

    third section

--- component/Contact.tsx
+++ component/Contact.tsx
@@ -3,54 +3,53 @@ import styles from "./../styles/Home.module.scss";
 
 const Contact = () => {
   return (
-    <section className={styles.contact}>
-      <div className={styles.mail}>
-        <div>
-          <p>CONTACT</p>
-          <h1>Let's work together</h1>
-          <button>Message</button>
+    <>
+      <section className={styles.contact}>
+        <div className={styles.mail}>
+          <div>
+            <p>CONTACT</p>
+            <h1>Let's work together</h1>
+            <button>Message</button>
+          </div>
+          <div>
+            <img src="email-box.png" />
+          </div>
         </div>
-        <div>
-          <img src="email-box.png" />
-        </div>
-      </div>
-      <section>
+      </section>
+      <section className={styles.socials}>
         <p>SOCIALS</p>
         <section className={styles.app}>
-          <button className={styles.github}>
-            <div>
-              <img src="github.png" />
-              <p>Github</p>
-            </div>
-            <img src="pink-arrow.png" />
-          </button>
-          <button className={styles.github}>
-            <div>
-              <img src="linkedin.png" />
-              <p>Linkedin</p>
-            </div>
+          <a href="https://github.com/miskiat" className={styles.github}>
+            <img src="github.png" />
+            <h3 className={styles.socialname}>Github</h3>
 
             <img src="pink-arrow.png" />
-          </button>
-          <button className={styles.github}>
-            <div>
-              <img src="codepen.png" />
-              <p>Codepen</p>
-            </div>
+          </a>
+          <a className={styles.github}>
+            <img src="linkedin.png" />
+            <h3 className={styles.socialname}>Linkedin</h3>
 
             <img src="pink-arrow.png" />
-          </button>
-          <button className={styles.github}>
-            <div>
-              <img src="twitter.png" />
-              <p>Twitter</p>
-            </div>
+          </a>
+          <a className={styles.github}>
+            <img src="codepen.png" />
+            <h3 className={styles.socialname}>Codepen</h3>
 
             <img src="pink-arrow.png" />
-          </button>
+          </a>
+          <a
+            href="https://twitter.com/MiskiatB"
+            target="_blank"
+            className={styles.github}
+          >
+            <img src="twitter.png" />
+            <h3 className={styles.socialname}>Twitter</h3>
+
+            <img src="pink-arrow.png" />
+          </a>
         </section>
       </section>
-    </section>
+    </>
   );
 };
 
--- component/Project.tsx
+++ component/Project.tsx
@@ -5,11 +5,18 @@ const Project = () => {
   return (
     <section className={styles.project}>
       <div>
-        <p>FEATURED PROJECTS</p>
+        <p className={styles.featured}>FEATURED PROJECTS</p>
         <h1>Projects I've Worked On 📁</h1>
         <section>
-          <div className=sty>
-
+          <div className={styles.projectcard}>
+            <img src="thelookbymis.b.png" />
+            <h3>ThelookbyMis.B</h3>
+            <p>
+              A fashion website to order and book for appointment and training
+            </p>
+            <button>Javascript</button>
+            <button>React</button>
+            <button>CSS</button>
           </div>
         </section>
       </div>
